144f85d4630c2c4de65c7fdfad6b068aa21b6e04
Robert-wdd <robert@wdd.nl>
2022-02-28 21:40:51 +0100

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -3,8 +3,37 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
+
+  public result: any = [];
+
+  constructor() {
+    this.getAllOrdersFromToday();
+
+    console.log('test');
+  }
+
+  public async getAllOrdersFromToday(): Promise<void> {
+    var requestOptions: any = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    this.result = fetch(
+      'https://tinlammysql.tinlam.repl.co/halenbrengen',
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        
+        const obj = JSON.parse(result);
+        console.log(obj);
+
+
+      })
+      .catch((error) => console.log('error', error));
+  }
 }
